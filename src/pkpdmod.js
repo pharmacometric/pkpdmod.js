e3ff15edb3d23d26199eb4e763f359108b0b3f92
William Hane <162056037+pharmacometric@users.noreply.github.com>
2024-08-13 15:28:32 -0400

    Update pkpdmod.js

--- src/pkpdmod.js
+++ src/pkpdmod.js
@@ -222,27 +222,80 @@ class PKPDmod {
 	}
 	
 	oneDepot({
-		x = 0,
-		y = 0,
+		cID,
+		x,
+		y,
+		boxw,
+		boxh,
+		linw,
+		radiz,
+		cushion,
 		circle = false,
 		labdose = "DOSE",
 		txtcol = "red",
 		bgcol = "white",
 		depottxt = "DEPOT1",
-		depottxt2 = "(0)",
-		depottxt3 = "DEPOT2",
-		depottxt4 = "(1)"
+		depottxt2 = "(0)"
 	}){
-		let cushion = 5;
-        let boxw = 150;
-        let boxh = 150;
-        let linw = 120;
-        let radiz = 6;
-        let depotx = x + cushion;
-        let depoty = y + cushion;
+        let depotx = x ;
+        let depoty = y;
 		
-		
-		
+		this.drwLineTB({
+                    cID: cID,
+                    x: depotx + (boxw / 2),
+                    y: cushion,
+                    length: linw,
+                    lc: "black",
+                    ahfc: "white",
+                    ahbc: "black",
+                    arp: "bottom",
+                    txtmid: "",
+                    topstr: labdose,
+                    coltxt: txtcol
+                }),
+                depotx += linw + boxw + 3;
+            depoty += linw + 20;
+            if (circle) {
+                this.drwcBox({
+                    x: cushion,
+                    y: depoty,
+                    radius: boxw / 2,
+                    lw: 4,
+                    bg: bgcol,
+                    txt: depottxt,
+                    txt2: "V2",
+                    txt2: depottxt2,
+                    coltxt: txtcol
+                })
+            } else {
+                this.drwBox({
+                    cID: cID,
+                    x: cushion,
+                    y: depoty,
+                    width: boxw,
+                    height: boxh,
+                    radius: radiz,
+                    color1: "black",
+                    linewidth: 2,
+                    color2: bgcol,
+                    text1: depottxt,
+                    text2: depottxt2,
+                    coltxt: txtcol
+                })
+            }
+            this.drwLineLR({
+                cID: cID,
+                x: boxw + cushion,
+                y: depoty + boxh/2,
+                open: !1,
+                arp: "right",
+                hpos: "under",
+                dash: !1,
+                l: linw,
+                txtmid: "Ka",
+                coltxt: txtcol
+            })
+		return [depotx, depoty];
 	}
 	
     drwLineST({
@@ -391,62 +444,28 @@ class PKPDmod {
         let radiz = 6;
         let depotx = cushion;
         let depoty = cushion;
+		
         if (depot) {
-            this.drwLineTB({
-                    cID: this.cID,
-                    x: depotx + (boxw / 2),
-                    y: cushion,
-                    length: linw,
-                    lc: "black",
-                    ahfc: "white",
-                    ahbc: "black",
-                    arp: "bottom",
-                    txtmid: "",
-                    topstr: labdose,
-                    coltxt: txtcol
-                }),
-                depotx += linw + boxw + 3;
-            depoty += linw + 20;
-            if (circle) {
-                this.drwcBox({
-                    x: cushion,
-                    y: depoty,
-                    radius: boxw / 2,
-                    lw: 4,
-                    bg: bgcol,
-                    txt: depottxt,
-                    txt2: "V2",
-                    txt2: depottxt2,
-                    coltxt: txtcol
-                })
-            } else {
-                this.drwBox({
-                    cID: this.cID,
-                    x: cushion,
-                    y: depoty,
-                    width: boxw,
-                    height: boxh,
-                    radius: radiz,
-                    color1: "black",
-                    linewidth: 2,
-                    color2: bgcol,
-                    text1: depottxt,
-                    text2: depottxt2,
-                    coltxt: txtcol
-                })
-            }
-            this.drwLineLR({
-                cID: this.cID,
-                x: boxw + cushion,
-                y: depoty + boxh/2,
-                open: !1,
-                arp: "right",
-                hpos: "under",
-                dash: !1,
-                l: linw,
-                txtmid: "Ka",
-                coltxt: txtcol
-            })
+        
+		let depotadd = this.oneDepot({
+			cID: this.cID,
+			x: depotx,
+			y: depoty,
+			boxw: boxw,
+			boxh: boxh,
+			linw: linw,
+			radiz: radiz,
+			circle: circle,
+			labdose: "DOSE",
+			txtcol: "red",
+			bgcol: "white",
+			depottxt: "DEPOT1",
+			depottxt2: "(0)",
+			cushion: cushion
+		});
+		
+		depotx = depotadd[0];
+		depoty = depotadd[1];
 
         }
         if (bolus) {
